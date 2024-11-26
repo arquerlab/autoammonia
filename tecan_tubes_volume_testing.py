9b202495015de86c589724e41dcddce2728e7cc1
apinilla <adpisa@gmail.com>
2024-11-26 17:15:09 -0500

    Initial version of potentiostat class Data is collected, tranformed to np.float32 and appended to a queue. Simultaneously another thread reads and saves this queue into a csv. Additionally, data can be averaged before appended to reduce data size

---
+++ tecan_tubes_volume_testing.py
@@ -0,0 +1,66 @@
+from Amonia_SDL_v02 import run_on_component, CONFIG_COMPONENTS, draw_and_dispense_tecan_func, switch_port_valve, draw_tecan_func, dispense_tecan_func
+from default_config import CONNECTIONS_INFO
+
+if __name__ == '__main__':
+    for component in CONNECTIONS_INFO:
+        skip_component = False
+        print(f'Component to use: {component}')
+        if 'tecan' in component:
+            pump = component
+            max_vol = CONFIG_COMPONENTS[component]['syringe_volume']*1000
+            print(max_vol)
+        if skip_component:
+            next
+        else:
+            for port in CONNECTIONS_INFO[component]:
+                if port != 'air_waste' and not skip_component:
+                    if 'valve' in component:
+                        for _ in range(3):
+                            try:
+                                switch_port_valve(component, port)
+                                break
+                            except:
+                                pass
+                    total_vol = 0
+                    print(f'Port to use: {port}')
+                    if port == 'valve':
+                        input('Disconnect tube from valve and put in an empty container. Press enter to continue.')
+                        switch_port_valve(component, 'air_waste')
+                    else:
+                        input(f'Fill container of {port} with water. Press enter to continue.')
+                    while True:
+                        vol = input(f'Indicate volume to fill up, or stop for moving forward (acum_vol = {total_vol}): ')
+                        if vol == 'stop' or vol == 's' or vol == 'skip':
+                            print(f'Port initialized. Total volume: {total_vol}')
+                            break
+                        elif 'next c' in vol or 'skip c' in vol:
+                            print('Skipping component')
+                            skip_component = True
+                            break
+                        else:
+                            try:
+                                vol = float(vol)
+                                if abs(vol) > max_vol:
+                                    print(vol, max_vol)
+                                    raise ValueError
+                                if vol>0:
+                                    if port == 'valve':
+                                        draw_and_dispense_tecan_func(pump, vol, 'water', port, speed=0.005)
+                                        total_vol += vol
+                                    else:
+                                        draw_port = port if 'valve' not in component else 'valve'
+                                        draw_tecan_func(pump,vol,draw_port,speed=0.0075)
+                                        dispense_tecan_func(pump,vol,'air_waste',speed=1000)
+                                        total_vol += vol
+                                if vol<0:
+                                    if port == 'valve':
+                                        draw_and_dispense_tecan_func(pump, abs(vol), port, 'air_waste', speed=1000)
+                                        total_vol = 0
+                                    else:
+                                        dispense_port = port if 'valve' not in component else 'valve'
+                                        draw_and_dispense_tecan_func(pump,abs(vol),'air_waste',dispense_port,speed=1000)
+                                        total_vol = 0
+                            except ValueError:
+                                print('Value exceeds the syringe limit, try again.')
+                            except Exception as e:
+                                print(Exception, e)
