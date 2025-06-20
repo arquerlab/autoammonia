c02a61895a31325f21dcb136f9be36cbbef2914c
apinilla <adpisa@gmail.com>
2025-06-20 11:30:49 -0400

    Adding entry point for easy access to the reaction module test script

--- src/autoammonia/testing/rx_module_test.py
+++ src/autoammonia/testing/rx_module_test.py
@@ -1,12 +1,13 @@
 from ..reaction_module import process_experiment_queue
 
-if __name__ == '__main__':
-    """
-    This script is designed to run the reaction module flow, processing tasks from the experiment queue.
-    It can be executed directly to start the flow.
-    """
+"""
+This script is designed to run the reaction module flow, processing tasks from the experiment queue.
+It can be executed directly to start the flow.
+"""
+def main():
     process_experiment_queue(delete_previous_queue=True,
                              electrodeposition_time=600, reaction_time=10, electrodisolution_time=10,
                              electrodeposition_current=-0.004, reaction_current=+0.004,
                              wash_flow_cell_repeats=1, wash_flow_cell_wash_comp_volume=2.5,
                              )
+    
