3a8bb4cbd77e1941192145535c5b45baf8ab5ea9
apinilla <adpisa@gmail.com>
2025-06-22 12:04:07 -0400

    Debugging

--- src/autoammonia/testing/rx_module_test.py
+++ src/autoammonia/testing/rx_module_test.py
@@ -6,7 +6,7 @@ It can be executed directly to start the flow.
 """
 def main():
     process_experiment_queue(delete_previous_queue=True,
-                             electrodeposition_time=600, reaction_time=10, electrodisolution_time=10,
+                             electrodeposition_time=10, reaction_time=10, electrodisolution_time=10,
                              electrodeposition_current=-0.004, reaction_current=+0.004,
                              wash_flow_cell_repeats=1, wash_flow_cell_wash_comp_volume=2.5,
                              )
