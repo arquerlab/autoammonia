3d9ff226d0b79b44cb4b607540e9ab24c82df2f1
apinilla <adpisa@gmail.com>
2025-06-23 23:47:39 -0400

    Debugging

--- src/autoammonia/testing/rx_module_test.py
+++ src/autoammonia/testing/rx_module_test.py
@@ -5,7 +5,7 @@ This script is designed to run the reaction module flow, processing tasks from t
 It can be executed directly to start the flow.
 """
 def main():
-    process_experiment_queue(delete_previous_queue=True,
+    process_experiment_queue(delete_previous_queue=False,
                              electrodeposition_time=10, reaction_time=10, electrodisolution_time=10,
                              wash_flow_cell_time=1, 
                              electrodeposition_current=-0.004, reaction_current=+0.004,
