de8d56e8aea83a0c72f4974ae54d5f9b60cdfc92
apinilla <adpisa@gmail.com>
2025-06-25 12:40:02 -0400

    Debugging

--- src/autoammonia/hardware/mock/lamp_mock.py
+++ src/autoammonia/hardware/mock/lamp_mock.py
@@ -34,7 +34,7 @@ class LampMock(ABC):
 
 # --- Mock MotorSwitchLamp ---
 class MotorSwitchLampMock(LampMock):
-    def __init__(self, arduino: ArduinoMock, on_degrees: int, off_degrees: int):
+    def __init__(self, arduino: ArduinoMock, on_degrees: int = 50, off_degrees: int = 0):
         super().__init__(arduino, on_degrees=on_degrees, off_degrees=off_degrees)
         self.arduino = arduino
         self.on_degrees = on_degrees
