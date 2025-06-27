e3ded976f7c3c346fd493067e57b7b87020de663
apinilla <adpisa@gmail.com>
2025-06-27 18:09:23 -0400

    Debugging

--- src/autoammonia/analysis_module.py
+++ src/autoammonia/analysis_module.py
@@ -118,7 +118,7 @@ def take_aliquots(
         time_rxn = aliquot_time - initial_reaction_time
         trigger_deployments(deployments=['measure_vial/measure_vial_uv_vis_flow',],
                             scheduled_time=measure_time, 
-                            parameters={'vial': vial, 'time_rxn': time_rxn, 'exp_id': exp_id, **kwargs})
+                            parameters={'vial': vial, 'time_rxn': time_rxn, 'exp_id': exp_id, 'kwargs': kwargs})
         logger.info(f'Programmed measurement of {vial} from {WEvial} at {measure_time}')
         
 
